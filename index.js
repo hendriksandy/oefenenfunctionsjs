eaade84f1e615b51489e4ba13a59d82cbbfd1738
hendriksandy <79459594+hendriksandy@users.noreply.github.com>
2021-04-23 00:21:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const paintJob = function (verfkleur) {
+    console.log("The wall has been painted " + verfkleur);
+
+};
+
+paintJob("geel");
+
+const paintDirection = function (windrichtingA, windrichtingB) {
+    console.log("The " + windrichtingA + " wall had been painted orange or The " + windrichtingB + " wall has been painted grey");
+
+};
+
+paintDirection("north", "south");
